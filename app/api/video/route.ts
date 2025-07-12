52f37b01ca4bd4a03ed8baf30cc16555cb1ac01e
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-07-13 01:53:30 +0530

    add imagekit things

---
+++ app/api/video/route.ts
@@ -0,0 +1,64 @@
+import { authOptions } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import Video, { IVideo } from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connectToDatabase();
+        const videos = await Video.find({}).sort({ createdAt: -1 }).lean();
+        
+        if (!videos || videos.length === 0) {
+            return NextResponse.json([],{status:500})
+        }
+        return NextResponse.json(videos)
+    } catch (error) {
+
+        return NextResponse.json(
+            { error: "Failed to fetch videos" },
+            {status:500}
+        )
+    }
+}
+
+export async function POST(request:NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json(
+                { error: "Unauthorized!!" },
+                { status: 400 }
+            )
+        }
+        await connectToDatabase();
+        const body: IVideo = await request.json();
+        if (!body.title ||
+            !body.description ||
+            !body.thumbnailUrl ||
+            !body.videoUrl) {
+            return NextResponse.json(
+                {error:"Missing required fields!"},{status:400}
+            )
+        }
+
+        const videoDate = {
+            ...body,
+            controls: body?.controls ?? true,
+            transformation: {
+                height: 1920,
+                width: 1080,
+                quality:body.transformation?.quality ??100
+            }
+        }
+
+        const newVideos = await Video.create(videoDate);
+        return NextResponse.json(newVideos)
+
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to create videos" },
+            { status: 500 }
+        )
+    }
+}
